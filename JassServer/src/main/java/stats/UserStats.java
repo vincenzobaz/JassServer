bf8bd865d129aa78bc0d552b8725556e27a6e7c9
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-22 18:03:47 +0100

    imports

--- JassServer/src/main/java/stats/UserStats.java
+++ JassServer/src/main/java/stats/UserStats.java
@@ -6,14 +6,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import model.Match;
 import model.Player;
